87b9654fd0b36a00d68b95f3762f975e59a14fab
miroslavrs81 <miroslav81ilic@gmail.com>
2023-11-11 21:02:00 +0100

    Conditionals and booleans

--- index.php
+++ index.php
@@ -2,14 +2,30 @@
 <html lang="en">
 <head>
     <title>Demo</title>
+    <style>
+      body {    
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    margin: 0;
+    font-family: sans-serif;
+      }
+</style>
 </head>
 <body>
+    <?php
+    $name = "Dark Matter";
+    $read = true;
+
+    if ($read) {
+        $message = "You have read $name";
+    } else {
+        $message = "You have NOT read $name";
+    }
+    ?>
     
 <h1> 
-    <?php
-    $greetings = "Hello";
-    echo "$greetings World";
-    ?>
+   <?= $message ?>
 </h1>
 
 </body>
