94724058e39e30f3501fcfc7491a8f24a9e77f8e
miroslavrs81 <miroslav81ilic@gmail.com>
2023-11-13 20:35:12 +0100

    Page Links

--- ourMission.view.php
+++ ourMission.view.php
@@ -19,10 +19,10 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-              <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
-              <a href="/contact.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
-              <a href="/ourMission.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Our Mission</a>
+              <a href="/" class="text-gray-300 hover:bg-gray-700 px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+              <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white text-gray-300 hover:bg-gray-700 px-3 py-2 text-sm font-medium">About</a>
+              <a href="/contact.php" class="text-gray-300 hover:bg-gray-700 hover:text-white text-gray-300 hover:bg-gray-700 px-3 py-2 text-sm font-medium">Contact</a>
+              <a href="/ourMission.php" class="bg-gray-900 text-white text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Our Mission</a>
             </div>
           </div>
         </div>
@@ -121,15 +121,15 @@
 
   <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Our Mission</h1>
     </div>
   </header>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <p>Hello. Welcome to the home page.</p>
+      <p>This is Our Mission!</p>
     </div>
   </main>
-</div>
+</div> 
 
 </body>
 </html>
