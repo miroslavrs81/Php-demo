0d766213bed4c293905a72dad57e8c07ee72fd9b
miroslavrs81 <miroslav81ilic@gmail.com>
2023-11-20 21:14:32 +0100

    Extact a PHP Database Class

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php
 
-require 'function.php';
-// require 'router.php';
+require 'functions.php';
+//require 'router.php';
+require 'Database.php';
 
+$db = new Database();
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$dns = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-
-$pdo = new PDO($dns);
-
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
