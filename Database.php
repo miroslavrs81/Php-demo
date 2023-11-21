d25a50efdf2211ecf36e01f9e5dda20f2382ed6e
miroslavrs81 <miroslav81ilic@gmail.com>
2023-11-21 12:07:34 +0100

    Enviroments and Configuration

--- Database.php
+++ Database.php
@@ -4,11 +4,12 @@ class Database
 {
     public $connection;
 
-    public function __construct()
+    public function __construct($config, $userName = 'root', $password = '')
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
 
-        $this->connection = new PDO($dsn);
+        $this->connection = new PDO($dsn, $userName, $password, [ 
+           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
     }
 
     public function query($query)
