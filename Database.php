7952cda26aa876e3738647955e44afbcbab388a6
miroslavrs81 <miroslav81ilic@gmail.com>
2023-11-22 21:55:57 +0100

    SQL Injection Vulnerabilities

--- Database.php
+++ Database.php
@@ -9,14 +9,15 @@ class Database
         $dsn = 'mysql:' . http_build_query($config, '', ';');
 
         $this->connection = new PDO($dsn, $userName, $password, [ 
-           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
-    public function query($query)
+    public function query($query, $params = [])
     {
         $statement = $this->connection->prepare($query);
 
-        $statement->execute();
+        $statement->execute($params);
 
         return $statement;
     }
--- index.php
+++ index.php
@@ -5,8 +5,11 @@ require 'functions.php';
 require 'Database.php';
 
 $config = require ('config.php');
-
 $db = new Database($config['database']);
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
+
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
