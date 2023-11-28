b35e37e13e7de38d8128cdb0ab8d7e4cbb2cc5c1
miroslavrs81 <miroslav81ilic@gmail.com>
2023-11-28 18:54:33 +0100

    Into to Form Validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -5,11 +5,24 @@ $db = new Database($config['database']);
 
 $heading = "Create Note";
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)',[
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $errors = [];
+
+    if(strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if(strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'A body can both be larger than 1000 characters';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
